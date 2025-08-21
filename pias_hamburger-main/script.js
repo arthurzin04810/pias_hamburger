8c8d9721e0b5fa6adc9e849f4f4ed6fd0de5e890
athzin04810 <arthur.hettwer@escola.pr.gov.br>
2025-08-21 08:45:40 -0300

    Add files via upload

---
+++ pias_hamburger-main/script.js
@@ -0,0 +1,19 @@
+const Lanches = [
+    {Id: "tudo", X: "X-tudo", Preço: R$ 27,99 },
+    {Id: "gaúcho", X: "X-gaúcho", Preço: R$ 29,99 },
+    {Id: "salada", X: "X-salada", Preço: R$ 24,99 },
+    {Id:"egg", X: "X-egg", Preço: R$ 27,99 },
+    {Id: "bacon", X: "X-bacon", Preço: R$ 27,99 },
+];
+
+
+const Bebidas = [
+    {Id: "uva", X: "suco de uva", Preço: R$ 10,00 },
+    {Id: "morango", X: "suco de morango", Preço: R$ 11.99 },
+    {Id: "maracujá", X: "suco e maracujá", Preço: R$ 12,99 },
+    {Id:"cajú", X: "suco de cajú", Preço: R$ 10,99 },
+    {Id: "ET", X: "suco ET", Preço: R$ 999,99 },
+];
+
+
+
