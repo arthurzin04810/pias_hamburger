bf3ceaa53ef3d1debd152b27ca67ede17faa33d0
athzin04810 <arthur.hettwer@escola.pr.gov.br>
2025-08-21 12:11:39 -0300

    Add files via upload

--- pias_hamburger-main/script.js
+++ pias_hamburger-main/script.js
@@ -1,19 +1,27 @@
 const Lanches = [
-    {Id: "tudo", X: "X-tudo", Preço: R$ 27,99 },
-    {Id: "gaúcho", X: "X-gaúcho", Preço: R$ 29,99 },
-    {Id: "salada", X: "X-salada", Preço: R$ 24,99 },
-    {Id:"egg", X: "X-egg", Preço: R$ 27,99 },
-    {Id: "bacon", X: "X-bacon", Preço: R$ 27,99 },
+    {Id: "tudo", X: "X-tudo", Preço: 27.99 },
+    {Id: "gaúcho", X: "X-gaúcho", Preço: 29.99 },
+    {Id: "salada", X: "X-salada", Preço: 24.99 },
+    {Id:"egg", X: "X-egg", Preço: 27.99 },
+    {Id: "bacon", X: "X-bacon", Preço: 27.99 }
 ];
 
 
 const Bebidas = [
-    {Id: "uva", X: "suco de uva", Preço: R$ 10,00 },
-    {Id: "morango", X: "suco de morango", Preço: R$ 11.99 },
-    {Id: "maracujá", X: "suco e maracujá", Preço: R$ 12,99 },
-    {Id:"cajú", X: "suco de cajú", Preço: R$ 10,99 },
-    {Id: "ET", X: "suco ET", Preço: R$ 999,99 },
+    {Id: "uva", X: "suco de uva", Preço: 10.00 },
+    {Id: "morango", X: "suco de morango", Preço: 11.99 },
+    {Id: "maracujá", X: "suco e maracujá", Preço: 12.99 },
+    {Id:"cajú", X: "suco de cajú", Preço: 10.99 },
+    {Id: "ET", X: "suco ET", Preço: 999.99 }
 ];
 
 
+function preencheOption(){
+const selectLanche = document.getElementById("transporte")
 
+
+
+}
+
+
+preencheOption();
