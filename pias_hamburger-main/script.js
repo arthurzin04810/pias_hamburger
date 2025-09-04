d5c4841a6b01671c3cbf93261401a5b2ef06d00d
athzin04810 <arthur.hettwer@escola.pr.gov.br>
2025-09-04 12:12:56 -0300

    Add files via upload

--- pias_hamburger-main/script.js
+++ pias_hamburger-main/script.js
@@ -1,36 +1,27 @@
 const Lanches = [
-    {Id: "tudo", X: "X-tudo", Preço:  27.99 },
-    {Id: "gaúcho", X: "X-gaúcho", Preço:  29.99 },
-    {Id: "salada", X: "X-salada", Preço:  24.99 },
-    {Id:"egg", X: "X-egg", Preço:  27.99 },
-    {Id: "bacon", X: "X-bacon", Preço:  27.99 },
-    {Id: "linguiça", X: "X-Pedrão Com Linguiça", Preço:  99.99 },
-    {Id: "Ovos", X: "X-Pedrão Com Ovos", Preço:  89.99 },
-    {Id: "Cheddar", X: "X-Pedrão Com Cheddar", Preço:  70.99 },
-    {Id: "Fritas", X: "X-Pedrão Com Fritas", Preço:  80.99 },
-    {Id: "Na Chapa", X: "X-Pedrão Na Chapa", Preço:  67.99 },
-    {Id: "Molho Pedrao", X: "X-Pedrão Ao Molho Branco", Preço:  97.99 },
-    {Id: "Molho Extra (150 ML)", X: "Molho do Pedrão (A Parte)", Preço:  5.99 },
-
-
-
+    {Id: "tudo", X: "X-tudo", Preço: 27.99 },
+    {Id: "gaúcho", X: "X-gaúcho", Preço: 29.99 },
+    {Id: "salada", X: "X-salada", Preço: 24.99 },
+    {Id:"egg", X: "X-egg", Preço: 27.99 },
+    {Id: "bacon", X: "X-bacon", Preço: 27.99 }
 ];
-
 
 
 const Bebidas = [
-    {Id: "uva", X: "suco de uva", Preço:  10.00 },
-    {Id: "morango", X: "suco de morango", Preço:  11.99 },
+    {Id: "uva", X: "suco de uva", Preço: 10.00 },
+    {Id: "morango", X: "suco de morango", Preço: 11.99 },
     {Id: "maracujá", X: "suco e maracujá", Preço: 12.99 },
-    {Id:"cajú", X: "suco de cajú", Preço:  10.99 },
-    {Id: "ET", X: "suco ET", Preço:  19.99 }, 
-    {Id: "Leite", X: "Leite Do Pedrão Com Nescau", Preço:  26.99 }, 
-    {Id: "Suco", X: "Suco Especial Pedrão", Preço:  19.99 }, 
-
-
-
-
+    {Id:"cajú", X: "suco de cajú", Preço: 10.99 },
+    {Id: "ET", X: "suco ET", Preço: 999.99 }
 ];
 
 
+function preencheOption(){
+const selectLanche = document.getElementById("transporte")
 
+
+
+}
+
+
+preencheOption();
